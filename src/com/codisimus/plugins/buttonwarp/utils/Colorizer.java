3e4126df4b8976a7388488ac690bb05a04520bcc
Spearhartt <taggertv@gmail.com>
2017-01-30 11:16:51 -0500

    Created Colorizer for configuring custom message colors.

---
+++ src/com/codisimus/plugins/buttonwarp/utils/Colorizer.java
@@ -0,0 +1,24 @@
+package com.codisimus.plugins.buttonwarp.utils;
+
+/**
+ * Created by Spearhartt on 1/30/2017.
+ */
+public class Colorizer {
+
+    public static String aColor;
+    public static String bColor;
+    public static String cColor;
+    public static String dColor;
+
+    public static void formatAll() {
+
+    }
+
+    public static String format(String string) {
+        return string.replace("&", "§").replace("<ae>", "æ")
+                .replace("<AE>", "Æ").replace("<o/>", "ø")
+                .replace("<O/>", "Ø").replace("<a>", "å")
+                .replace("<A>", "Å");
+    }
+}
+
